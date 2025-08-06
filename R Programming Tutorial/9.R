91076dff627203233f42118d7878a27eb6bc3154
Balasubramanian PG <102461377+Balasubramanian-pg@users.noreply.github.com>
2025-08-06 22:20:26 +0530

    Update and rename 9.R to 9. 3D Pie Charts in R.md

--- R Programming Tutorial/9.R
+++
@@ -1,6 +0,0 @@
-#3d pie chart
-#plotrix
-install.packages("plotrix")
-library(plotrix)
-pie3D(x,labels=pie_percentage,main="Population",explode = 0.2)
-legend("topleft",label,cex=1,fill=colour) #cex is a size of label
